0b9a6b0ec938195a94b2fe5784a942c47a794649
shreeharikulkarni19112001 <63485579+shreeharikulkarni19112001@users.noreply.github.com>
2020-04-11 09:58:12 +0530

    Update sum and average of n numbers.c

--- sum and average of n numbers.c
+++ sum and average of n numbers.c
@@ -22,4 +22,17 @@ int main()
 	printf("average of n numbers is %f \n",average);
 }
 	
+OUTPUT
+enter the value of n
+5
+enter the elements of array
+1 2 3 4 5
+sum of n numbers is 15
+average of n numbers is 3.000000
+
+
+------------------
+(program exited with code: 0)
+
+Press any key to continue . . .
 	
